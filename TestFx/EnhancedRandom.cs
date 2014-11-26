a3eaf26c243662d8f887fb91ad998a15b04483b4
First Last <e.mail@ddre.ss>
2014-11-26 17:52:47 -0600

    initial test setup

---
+++ TestFx/EnhancedRandom.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFx
+{
+    public static class EnhancedRandom
+    {
+        public static bool RandomBool()
+        {
+            var rnd = new Random();
+            var yuh = rnd.Next(0, 2);
+            return yuh == 1;
+        }
+
+        public static string RandomWords(int count)
+        {
+            var sb = new StringBuilder();
+            var seed = new Random();
+            for (var i = 0; i <= count; i++)
+            {
+                sb.Append(Words[seed.Next(1, Words.Length)] + " ");
+            }
+            return sb.ToString();
+        }
+
+        private static string[] Words =
+        {
+            "abacus", "beach", "corpulent", "dromedary", "eclipse", "flames", "giraffe", "hat", "ink", "jumping",
+            "kitsch", "lampshade", "monument", "ne'er-do-well", "octopus", "pineapple",
+            "quorum", "resurrection", "satisfied", "turbulence", "unify", "viable", "walk", "x-ray", "yams", "zydeco",
+            "attenuation", "biscuits", "crochet", "deleterious", "euphonium", "fragile", "gubernatorial", "hijack",
+            "importance", "judicious", "kibbutz", "languish", "monotone", "nope", "orienteering", "quail", "risotto",
+            "squeegee", "trampoline", "utilitarianism", "visceral", "wallbanger", "xenophobia", "yolo", "zoroastrianism"
+        };
+
+    }
+
+}
+
+
+
---
+++ TestFxDriver/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestFx;
+
+namespace TestFxDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(EnhancedRandom.RandomWords(50));
+            for (var i = 0; i < 20; i++)
+            {
+                Console.WriteLine(EnhancedRandom.RandomBool());
+            }
+            Console.ReadLine();
+        }
+    }
+}
